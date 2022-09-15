2cc94353d009615392d64101b075b25aafdc459c
Alec Kozlov <44117836+TheTimeShare@users.noreply.github.com>
2022-09-15 12:47:43 -0700

    Add files via upload

---
+++ Day 1/Contents.swift
@@ -0,0 +1,26 @@
+import Cocoa
+
+let actor = "Jennifer Aniston"
+let filename = "filename.swift"
+let result = "⭐️ You win! ⭐️"
+
+let quote = "Then he tapped a sign \"believe\" and walked away"
+
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+print(actor.count)
+
+let namelength = actor.count
+print(namelength)
+
+print(result.uppercased())
+
+print(movie.hasPrefix("A day"))
+
+print(filename.hasSuffix(".swift"))
+
