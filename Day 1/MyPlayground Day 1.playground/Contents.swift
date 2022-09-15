380ce3acfa3a735ca52a6d1008504b6739df6dc2
Alec Kozlov <44117836+TheTimeShare@users.noreply.github.com>
2022-09-15 12:18:20 -0700

    Add files via upload

---
+++ Day 1/MyPlayground Day 1.playground/Contents.swift
@@ -0,0 +1,28 @@
+var greeting = "Hello everyone!"
+
+// "var" is creating a new changeable variable.
+// You wont need to to type "var" infront since its only use to create new variables.
+var name = "larry"
+name = "Harry"
+name = "Marry"
+
+// "let" wont let us change the variable. let is a constant.
+let character = "Tommy"
+
+
+
+var playerName = "Tom"
+print(playerName)
+
+playerName = "Tim"
+print(playerName)
+
+playerName = "john"
+print(playerName)
+
+
+let managerName = "Emil Johansen"
+let catName = "Milo"
+let bestOS = "MacOS is the best OS"
+
+
