9ac9cda064ab9194a6022317a09d385e4ae640f1
Alec Kozlov <44117836+TheTimeShare@users.noreply.github.com>
2022-09-16 22:29:12 +0200

    Create summary: simple data.swift

---
+++ Day 2/summary: simple data.swift
@@ -0,0 +1,29 @@
+// swift lets us create constants "let" and variables "var"
+// if you dont instend to change the value of a variable 
+// make sure to use "let" and make it a constant
+
+// swifts strings contain text, from short strings up to whole novels. 
+// they work great with both emoji and any world languages
+// and have a helpful functionality such as "count" and "uppercased()"
+
+// you create strings using double quotes at the start and end
+// but if you want to have your string go over multiple lines you use triple quotes """
+
+// swift calls its whole numbers integers and they can be positive and negative
+// they also have helpful functionality such as "isMultiple(of:int)"
+
+// in swift decimal numbers are called Double
+// short for double-length floating-point number.
+// that means they can hold very large numbers if needed
+// but they also aren't 100% accurate. 
+// you shouldnt use them when 100% precision is required
+// such as dealing with money
+
+// there are lotf of build-in arithmetic operators such as "+, -, * and /"
+// along with special compound assignment operators such as "+=" that modify variablers directly
+
+// you can represent a simple true or false statement by using boolean
+// which can be flipped either by using the "!" operator or "toggle()"
+
+// string interpolation lets us place constants and variables into our strings
+// in a streamlined efficient way
