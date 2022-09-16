d1656349aaae0e2df539349aeacfaa43c15c9e6a
Alec Kozlov <44117836+TheTimeShare@users.noreply.github.com>
2022-09-16 22:49:10 +0200

    Create Celsius to Fahrenheit.swift

---
+++ Checkpoints/Checkpoint 1/Celsius to Fahrenheit.swift
@@ -0,0 +1,16 @@
+var temperatureCelcius = 18.0 // temperature in Celcius
+var temperatureFahrenheit = 0.0 // temperature in Fahrenheit
+
+temperatureFahrenheit = temperatureCelcius * 9 // new value to "var temperatureFahrenheit" since we times it by 9
+temperatureFahrenheit = temperatureFahrenheit / 5 // new value to "var temperatureFahrenheit" since we devide it by 5
+temperatureFahrenheit = temperatureFahrenheit + 32 // new value to "var temperatureFahrenheit" since we add 32
+
+print(temperatureFahrenheit) // personal check to see if the variable changed
+print(temperatureCelcius) // personal check to see if the variable changed
+
+print("\(temperatureCelcius)° in Celcius is \(temperatureFahrenheit)° in Fahrenheit") 
+
+// output: 
+64.4
+18.4
+18.0° in Celcius is 64.4° in Fahrenheit
